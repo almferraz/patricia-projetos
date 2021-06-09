4fa253986f69089b5de73e06aa40881b6e342a25
André Ferraz <almferraz@gmail.com>
2021-06-08 21:10:31 -0300

    menu suave mobile

--- js/script.js
+++ js/script.js
@@ -74,24 +74,29 @@ btArrayBotoes.forEach(function (obj, index) {
 });
 
 // Scroll Suave para links Internos
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-  section.scrollIntoView();
+    // console.log(href);
+    //forme alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
 
-  // console.log(href);
-  //forme alternativa
-  // const topo = section.offsetTop;
-  // window.scrollTo({
-  //   top: topo,
-  //   behavior: 'smooth',
-  // });
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
-
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSection);
-});
+initScrollSuave();
